61f628f48bddead1096df6e3bf2154ae305a8aaf
rladk <rladkfk678@naver.com>
2021-03-30 21:11:46 +0900

    menu 파일

---
+++ burger/src/main/java/com/one/burger/entity/Menu.java
@@ -0,0 +1,13 @@
+package com.one.burger.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Menu {
+	private int menu_no;
+	private String menu_name;
+	private int menu_price;
+	private String menu_etc;
+}
