2810f28bd6067085b89e2e2455d69565e98f7891
LEE SJ <72378132+LSJ-KH@users.noreply.github.com>
2021-04-21 21:45:07 +0900

    ????

--- burger/src/main/java/com/one/burger/repository/MenuRepository.java
+++ burger/src/main/java/com/one/burger/repository/MenuRepository.java
@@ -12,7 +12,6 @@ import com.one.burger.entity.Menu;
 import com.one.burger.entity.MenuBranchVO;
 import com.one.burger.entity.MenuPhoto;
 import com.one.burger.entity.MenuPhotoVO;
-import com.one.burger.entity.SalesSuperTotal;
 
 @Repository
 public class MenuRepository {
